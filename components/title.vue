c1f1d67b7a45065636b95c070acc941acd4cfea5
radish <xf.radish@gmail.com>
2017-06-27 15:52:24 +0800

    Initial commit

---
+++ components/title.vue
@@ -0,0 +1,17 @@
+<style lang="less" scoped="">
+  .name {
+    color: #ffdddd;
+    font-size: 24px;
+    span {
+      color: #f00;
+    }
+  }
+</style>
+
+<template>
+  <div class="name">
+    not a  bar
+    <span>! good~</span>
+  </div>
+</template>
+
